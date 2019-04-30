e69cc6daa8bda4abc2194e9e8b51125c41ec3667
awvdeker <alexander.vandekerckhove@gmail.com>
2019-04-30 14:35:10 +0200

    loading icon and scrolling up to the top

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ AJAX-dynamic-loading
 
 - using bootstrap for the css styling and navbar
 
-- index.js: local AJAX request and handling the response if a link is clicked
+- index.js: local AJAX request and handling the response if a link is clicked: with new XMLHttpRequest()
+
+- loading icon while pages loaded: doesn't appear because loading is too fast: with setTimeout icon stays for .5 seconds
+
+- scroll up to the top of the window when link is clicked before page loading
--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var links = document.getElementsByTagName('a');
 
 for (let link of links){
   link.addEventListener('click',function(){
+    //scroll up to the to of the window
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
     //start loading icon
+    var spinner = document.createElement("i");
+    spinner.classList.add("fas","fa-spinner","fa-spin","ml-2");
+    link.parentNode.parentNode.parentNode.appendChild(spinner);
 
     ajaxLoading(link.id+'.html');
   });
@@ -14,7 +21,10 @@ function ajaxLoading(url){
   xhr.onload = function () {
     document.getElementById('contentHolder').innerHTML = xhr.responseText;
     //stop loading icon
-
+    setTimeout(function(){
+      var spinner = document.querySelector('.fas');
+      spinner.parentNode.removeChild(spinner);
+    },500);
   };
 
   xhr.open('GET',url,true);
